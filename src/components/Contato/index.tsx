a42d19ffc7ddea9472f84655f5e63e492f17a1b2
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-06-30 22:09:41 -0300

    sem mapa

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -3,7 +3,7 @@ export function Contato() {
     <div className="w-full py-10 ">
       <div className="max-w-5xl  mx-auto py-12 flex flex-col-reverse md:flex-row ">
         <div className="flex-1 px-5">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14615.145541769607!2d-46.44891935!3d-23.683595999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1656610192037!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58468.21573780845!2d-46.475962298615!3d-23.66653366536846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce69549e48e43f%3A0xe210627a68fc88df!2zTWF1w6EsIFNQ!5e0!3m2!1spt-BR!2sbr!4v1656637715014!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        */}
         </div>
         <div className="flex-1 px-5 p-10 md:p-0 ">
           <ul>
